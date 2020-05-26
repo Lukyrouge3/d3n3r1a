208c5db11f3c5435c21d099df0f28fb545b8c670
LukySama <lukyroug4@gmail.com>
2020-05-27 00:48:33 +0200

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,13 @@ const request = require('snekfetch');
 /* GET home page. */
 
 router.get('/', async function (req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    nekos: (await request.get('https://nekos.life/api/v2/cat')).body.cat
-  });
+    request.get('https://nekos.life/api/v2/cat').then(n => {
+        res.render('index', {
+            title: 'Express',
+            nekos: n.body.cat
+        }, abort => {
+          res.render(abort);
+        });
+    });
 });
 module.exports = router;

0b8a2e83eb8b51a47f59a9f1991aadd267b425a4
LukySama <lukyroug4@gmail.com>
2020-05-29 13:33:28 +0200

    Initial commit

---
+++ apis/cooldown.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+module.exports = class CoolDown {
+    constructor() {
+        this.cooldown = new Discord.Collection();
+        // user.id, [commands]
+    }
+
+    async addCoolDown(user, command, time = 10000) {
+        return new Promise(resolve => {
+            let cd = this.cooldown.get(user.id) || [];
+            cd.push(command);
+            this.cooldown.set(user.id, cd);
+            setTimeout(() => {
+                this.removeCoolDown(user, command);
+                resolve(true);
+            }, time);
+        });
+    }
+
+    async removeCoolDown(user, command) {
+        let cd = this.cooldown.get(user.id) || [];
+        for (let i = 0; i < cd.length; i++) if (cd[i] === command) cd.splice(i, 1);
+        this.cooldown.set(user.id, cd);
+    }
+
+    isOnCoolDown(user, command) {
+        console.log(this.cooldown);
+        let cd = this.cooldown.get(user.id) || [];
+        for (let i = 0; i < cd.length; i++) if (cd[i] === command) return true;
+        return false;
+    }
+};
--- events/message.js
+++ events/message.js
@@ -1,33 +1,36 @@
 const COMMAND_PREFIX = process.env.PREFIX;
 
 const Nekos = require('../apis/nekos');
+const CoolDown = new (require('../apis/cooldown'))();
 const database = require('../apis/database');
 
 exports.run = async (client, msg) => {
-  // if (msg.author.bot) return;
-  const mentionanwsers = ["Stop mentioning me ! Im quite busy right now !", "Im not your girlfriend stop talking to me !", "Im not a toy im currently being cute so stop pinging me !"];
+    // if (msg.author.bot) return;
+    const mentionanwsers = ["Stop mentioning me ! Im quite busy right now !", "Im not your girlfriend stop talking to me !", "Im not a toy im currently being cute so stop pinging me !"];
 
-  if (msg.content.startsWith(COMMAND_PREFIX)) {
-    let msgArr = msg.content.split(" ");
-    let cmd = msgArr[0].slice(COMMAND_PREFIX.length);
-    let args = msgArr.slice(1);
-    let cmdFile = client.commands.get(cmd);
-    if (!cmd) return;
+    if (msg.content.startsWith(COMMAND_PREFIX)) {
+        let msgArr = msg.content.split(" ");
+        let cmd = msgArr[0].slice(COMMAND_PREFIX.length);
+        let args = msgArr.slice(1);
+        let cmdFile = client.commands.get(cmd);
+        if (!cmd) return;
 
-    if (cmdFile === undefined) {
-      msg.reply(await Nekos.owoify("I don't know this command !"));
-      return;
+        if (cmdFile === undefined) {
+            msg.reply(await Nekos.owoify("I don't know this command !"));
+            return;
+        }
+
+        if (cmdFile.isNSFW !== undefined && cmdFile.isNSFW(cmd) && !msg.channel.nsfw) {
+            msg.reply('This command must be used in a NSFW channel to protect 11years old people !');
+            return;
+        }
+        if (!CoolDown.isOnCoolDown(msg.author, cmd)) {
+            cmdFile.run(client, msg, args);
+            database.addCommand(cmd, msg);
+            await CoolDown.addCoolDown(msg.author, cmd, cmd.coolDown);
+        } else msg.reply('You\'re on cooldown');
+    } else if (msg.mentions.has(client.user)) {
+        console.log("I got mentionned !");
+        if (msg.author.id === process.env.OWNER_ID) msg.reply(await Nekos.owoify("What do you want master ?")); else msg.reply(await Nekos.owoify(mentionanwsers[Math.floor(Math.random() * mentionanwsers.length)]));
     }
-
-    if (cmdFile.isNSFW !== undefined && cmdFile.isNSFW(cmd) && !msg.channel.nsfw) {
-      msg.reply('This command must be used in a NSFW channel to protect 11years old people !');
-      return;
-    }
-
-    cmdFile.run(client, msg, args);
-    database.addCommand(cmd, msg);
-  } else if (msg.mentions.has(client.user)) {
-    console.log("I got mentionned !");
-    if (msg.author.id === process.env.OWNER_ID) msg.reply(await Nekos.owoify("What do you want master ?"));else msg.reply(await Nekos.owoify(mentionanwsers[Math.floor(Math.random() * mentionanwsers.length)]));
-  }
 };

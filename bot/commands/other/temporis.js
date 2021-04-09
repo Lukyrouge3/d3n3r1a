65e7d1927496609e6973bdc9cccbaf25f52547be
LukySama <lukyroug4@gmail.com>
2021-04-09 23:33:36 +0200

    Updated connectFour

--- bot/commands/other/temporis.js
+++ bot/commands/other/temporis.js
@@ -1,14 +1,14 @@
 const Nekos = require('../../apis/nekos');
 const moment = require('moment');
-const release = moment('2020-06-08 21:00');
+const release = moment('2021-04-21 18:00');
 exports.run = async (client, msg, args) => {
     let diff = release.diff(moment(), 'hours');
     let diff_day = release.diff(moment(), 'days');
     let str = "";
     if (diff < 0)
-        str = "Le serveur à commencé depuis " + diff + "h (" + diff_day + " jours)";
+        str = "Le serveur à commencé depuis " + Math.abs(diff) + "h (" + Math.abs(diff_day) + " jours)";
     else
-        str = "Le serveur commence dans " + diff + "h (" + diff_day + " jours)";
+        str = "Le serveur ouvre dans " + diff + "h (" + diff_day + " jours)";
     msg.reply(str);
 };
 

59fe3459e25114ed66328b157d71e83281c0778a
LukySama <lukyroug4@gmail.com>
2021-08-20 12:25:00 +0200

    Adding twitch alert

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const fs = require("fs");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const Uptime = new (require('./bot/apis/uptime'))(Date.now());
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const webhookRouter = require('./routes/webhook');
 
 require('dotenv').config();
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use((req, res, next) => {
+    fs.writeFileSync("birthdays.json", "");
+    next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/webhook', webhookRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -33,16 +41,15 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    // res.locals.message = err.message;
+    // res.locals.error = req.app.get('env') === 'development' ? err : {};
+    //
+    // // render the error page
+    // res.status(err.status || 500);
+    // res.render('error');
 });
 
 
-module.exports = app;
 
 const Bot = new (require('./bot/bot').Bot)();
 module.exports.bot = Bot;
@@ -51,3 +58,4 @@ let io = require('socket.io').listen(app.listen(process.env.PORT || 5000));
 let socket = require('./bot/socket');
 socket.run(io);
 
+module.exports = Bot;
---
+++ routes/webhook.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const Bot = require("../app.js");
+// const Discord = require("discord.js");
+// const Nekos = require("../bot/apis/nekos.js");
+const router = express.Router();
+/* GET users listing. */
+
+router.all('/callback', async function (req, res, next) {
+    (await Bot.bot.client.guilds.fetch('490955681197981715')).channels.cache.get('612355291010564104').send("Skyloudtv is online here: https://www.twitch.tv/skyloudlol");
+    (await Bot.bot.client.guilds.fetch('714232671089459282')).channels.cache.get('714232671840108568').send("Skyloudtv is online here: https://www.twitch.tv/skyloudlol");
+    res.send(req.body.challenge);
+});
+module.exports = router;

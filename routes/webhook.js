166871223b6cc942374b8129b287d3513f7b1479
LukySama <lukyroug4@gmail.com>
2021-08-20 12:26:07 +0200

    Adding twitch alert

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,8 +6,10 @@ const router = express.Router();
 /* GET users listing. */
 
 router.all('/callback', async function (req, res, next) {
-    (await Bot.bot.client.guilds.fetch('490955681197981715')).channels.cache.get('612355291010564104').send("Skyloudtv is online here: https://www.twitch.tv/skyloudlol");
-    (await Bot.bot.client.guilds.fetch('714232671089459282')).channels.cache.get('714232671840108568').send("Skyloudtv is online here: https://www.twitch.tv/skyloudlol");
-    res.send(req.body.challenge);
+    if (req.body.type === "stream.online") {
+        (await Bot.bot.client.guilds.fetch('490955681197981715')).channels.cache.get('612355291010564104').send("Skyloudtv is online here: https://www.twitch.tv/skyloudlol");
+        (await Bot.bot.client.guilds.fetch('714232671089459282')).channels.cache.get('714232671840108568').send("Skyloudtv is online here: https://www.twitch.tv/skyloudlol");
+        res.send(req.body.challenge);
+    }
 });
 module.exports = router;
